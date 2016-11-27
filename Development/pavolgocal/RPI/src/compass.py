c3ed2f06a4d30b96280bae3ad498ec76cd3c9b74
Pavol Gocal <pavol.gocal@centrum.sk>
2016-11-27 17:14:23 +0100

    zakladna struktura rpi

---
+++ Development/pavolgocal/RPI/src/compass.py
@@ -0,0 +1,16 @@
+#Class for compass data
+class compass:
+    #Properties
+
+    prop1 = 0;
+    prop2 = 0;
+    prop3 = 0;
+
+    #Constructor
+    #@prop1, property
+    #@prop2, property
+    #@prop3, property
+    def __init(self, prop1, prop2, prop3):
+        self.prop1 = prop1;
+        self.prop2 = prop2;
+        self.prop3 = prop3;
---
+++ Development/pavolgocal/RPI/src/controlUnit.py
@@ -0,0 +1,3 @@
+_author_ = 'pavolgocal'
+
+print("hello world")
