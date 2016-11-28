9a6218c79f2995013130338025bc2e099af0e121
martin <ilavskymartinn@gmail.com>
2016-11-28 17:54:19 +0100

    Zakladna struktura aplikacie na Raspberry PI. Obsahuje zakladne datove typy, vytvorenie dvoch vlakien a simulaciu prepisovania dat v datovych typov a vypisovanie ich aktualnej hodnoty.

---
+++ Main/Raspberry_1/gps.py
@@ -0,0 +1,12 @@
+#Class for gps data
+class Gps:
+
+    #Constructor
+    def __init__(self):
+        self.degree = 0.0
+
+
+    # Degree setter
+    # @degree, degree to set
+    def setDegree(self, degree):
+        self.degree = degree
