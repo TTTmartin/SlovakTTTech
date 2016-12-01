d1589b1d14ab642b555874fedb3dd5ab5566673b
martin <ilavskymartinn@gmail.com>
2016-12-01 14:06:13 +0100

    Funkcia na najdenie najblizsich uhlov teraz vracia list s dvoma uhlami, pricom prvy je ten blizsi.

--- Development/martinilavsky/riadenie/controlUnit.py
+++ Development/martinilavsky/riadenie/controlUnit.py
@@ -17,30 +17,31 @@ def steering_control():
         print(gps1.degree)
         time.sleep(1)
 
-# Finds closest clear degree to direction degree
-# returns int degree, if there are 2 same closest degrees, then returns list
+
+# Finds closest clear degrees to direction degree
+# returns 2 fields with degree, first is the closer one
 #@direction, main degree
 #@range_list, list of degrees 0-359 1-clear 0-blocked
-def find_closest_degree(direction,range_list):
-    left=0
-    right=0
+def find_closest_degree(direction, range_list):
+    left = 0
+    right = 0
+
+    x = direction
+    while range_list[x] != 1:
+        right += 1
+        x = (x+1) % 360
 
     x=direction
-    while (range_list[x] != 1):
-        right+=1
-        x=(x+1)%360
-
-    x=direction
-    while (range_list[x] != 1):
-        left+=1
-        x=(x-1)%360
+    while range_list[x] != 1:
+        left += 1
+        x = (x-1) % 360
 
     if(right<left):
-        return (direction+right)%360
+        return [(direction+right) % 360, (direction-left) % 360]
     elif (left<right):
-        return (direction-left)%360
+        return [(direction-left) % 360, (direction+right) % 360]
     else:
-        return [(direction+right)%360,(direction-left)%360]
+        return [(direction+right) % 360, (direction-left) % 360]
 
 
 #testing
